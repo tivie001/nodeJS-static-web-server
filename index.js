022a06f2cdeabebe9caafbe63842c0ec88140c17
Tyler Ivie <tylerivie@MacBook-Pro.local>
2020-10-02 16:21:49 -0600

    Refactored routes.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static('public'));
 
 app.listen(process.env.PORT || 3003, () => {
     console.log("Server running on port 3003");
