f66ce4bdfa2bb2f0759fbcdb6bb2afa767fb195c
Tyler Ivie <tylerivie@MacBook-Pro.local>
2020-10-02 16:53:21 -0600

    Refactored express static function to serve the right file.

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('public'));
+app.get('/', (req, res) => {
+    res.sendFile('index.html', {root: __dirname + '/public/'});
+});
 
 app.listen(process.env.PORT || 3003, () => {
     console.log("Server running on port 3003");
