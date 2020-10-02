1fb5104d0cbc8d881d1aa5a70d1f81b92728c0c6
Tyler Ivie <tylerivie@MacBook-Pro.local>
2020-10-02 16:17:35 -0600

    working on route.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + 'public'));
 
 app.listen(process.env.PORT || 3003, () => {
     console.log("Server running on port 3003");
