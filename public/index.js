591865a806e2a92fde38b787b0e9035b06e4c33d
Tyler Ivie <tylerivie@MacBook-Pro.local>
2020-10-02 17:02:06 -0600

    Refactored index.js to find the right file.

--- public/index.js
+++ public/index.js
@@ -2,9 +2,6 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('public'));
-app.get('/', (req, res) => {
-    res.sendFile('index.html', {root: '/public/'});
-});
 
 app.listen(process.env.PORT || 3003, () => {
     console.log("Server running on port 3003");
