6f9f40d58ed7d473f898678d3b9a3cd2427510b8
Tyler Ivie <tylerivie@MacBook-Pro.local>
2020-10-02 16:58:35 -0600

    Refactored index.js to find the right file.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.static('public'));
 app.get('/', (req, res) => {
-    res.sendFile('index.html', {root: __dirname + '/public/'});
+    res.sendFile('index.html', {root: '/public/'});
 });
 
 app.listen(process.env.PORT || 3003, () => {
